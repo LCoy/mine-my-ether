1ba73e571cd632ea6283ab36510c9b8d80f58c95
James <jmy9605@gmail.com>
2021-03-29 19:20:04 +0800

    update

--- windows.bat
+++ windows.bat
@@ -1,2 +1,3 @@
+@echo off
 bin/ethminer_0.18.0.exe -P stratum1+tcp://0xc538EeEdb560aF038ab3E93E9FEaD402e16e82e8.worker@asia1.ethermine.org:4444
 pause
---
+++ windows_binance.bat
@@ -0,0 +1,3 @@
+@echo off
+bin/ethminer_0.18.0.exe -P stratum+tcp://o123o.001@ethash.poolbinance.com:1800
+pause
